eab7d727c7c99b2ade5995217967fc2774d828f5
Manjunathan <manjunathan.jayakumar@mallow-tech.com>
2023-09-22 13:33:13 +0530

    reset_message_form, fix_scrollbar, fix_rooms_not_deleted_with_users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   scope :all_except, ->(user) { where.not(id: user)}
+  before_destroy :destroy_rooms
   has_many :messages, dependent: :destroy
   has_many :participants, dependent: :destroy
   has_many :rooms, through: :participants
+
+
+  private
+  def destroy_rooms
+    rooms.each do |room|
+      room.destroy
+    end
+  end
+
 end
